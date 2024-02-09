44d3ed7925818b57eed67662f447e45baeb5f770
therealmudd <m.antonio0294@gmail.com>
2024-02-09 10:12:46 +0200

    Code clean-up, antialiasing, diffuse materials

--- classes.js
+++ classes.js
@@ -1,3 +1,126 @@
+class Camera{
+  constructor(){
+    this.aspect_ratio = 1.0; // Ratio of image width over height
+    this.image_width = 100; // Rendered image width in pixel count
+    this.samples_per_pixel = 40; // Count of random samples for each pixel
+  }
+  
+  initialize(){
+    
+    this.aspect_ratio = 16 / 9;
+    this.image_width = 400;
+
+    // Calculate the image_height, and ensure that it's at least 1
+    this.image_height = Math.floor(this.image_width / this.aspect_ratio);
+    this.image_height = (this.image_height < 1) ? 1: this.image_height;
+
+    this.focal_length = 1.0;
+    this.viewport_height = 2.0;
+    this.viewport_width = this.viewport_height * (this.image_width / this.image_height);
+    this.center = new p5.Vector(0, 0, 0);
+
+    // Calulate the vectors across the horizontal and down the vertical viewport edges
+    this.viewport_u = new p5.Vector(this.viewport_width, 0, 0);
+    this.viewport_v = new p5.Vector(0, -this.viewport_height, 0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel
+    this.pixel_delta_u = p5.Vector.div(this.viewport_u, this.image_width);
+    this.pixel_delta_v = p5.Vector.div(this.viewport_v, this.image_height);
+
+    // Calculate the location of the upper left pixel
+    this.viewport_upper_left = subVecs(
+      this.center,
+      new p5.Vector(0, 0, this.focal_length),
+      p5.Vector.div(this.viewport_u, 2),
+      p5.Vector.div(this.viewport_v, 2)
+    );
+
+    this.pixel00_loc = p5.Vector.add(
+      this.viewport_upper_left, 
+      p5.Vector.mult( p5.Vector.add(this.pixel_delta_u, this.pixel_delta_v), 0.5 )
+    );
+  }
+  
+  render(world){
+    this.initialize();
+    
+    createCanvas(this.image_width, this.image_height);
+
+    pixelDensity(1);
+    loadPixels();
+    for (let j = 0; j < this.image_height; j ++){
+      for (let i = 0; i < this.image_width; i ++){
+        
+        let pixel_color = new p5.Vector(0, 0, 0);
+        for (let sample = 0; sample < this.samples_per_pixel; sample++){
+          let r = this.get_ray(i, j);
+          pixel_color.add(this.ray_color(r, world));
+        }
+        
+        let scl = 1.0 / this.samples_per_pixel;
+
+        let index = (j * width + i) * 4;
+        pixels[index + 0] = pixel_color.x * 255 * scl;
+        pixels[index + 1] = pixel_color.y * 255 * scl;
+        pixels[index + 2] = pixel_color.z * 255 * scl;
+        pixels[index + 3] = 255;
+      }
+    }
+    updatePixels();
+  }
+  
+  ray_color(r, world){
+    let rec = world.hit(r);
+    if (rec.hitAnything){
+      let direction = random_on_hemisphere(rec.nrml);
+      
+      return p5.Vector.mult(
+        this.ray_color(new Ray(rec.p, direction), world),
+        0.5
+      );
+      
+      // return p5.Vector.mult(
+      //   p5.Vector.add(rec.nrml, new p5.Vector(1, 1, 1)),
+      //   0.5
+      // );
+    }
+
+    let unit_direction = p5.Vector.normalize(r.direction);
+    let a = 0.5 * (unit_direction.y + 1);
+    return p5.Vector.lerp(
+      new p5.Vector(1.0, 1.0, 1.0),
+      new p5.Vector(0.5, 0.7, 1.0), 
+      a
+    );
+  }
+  
+  get_ray(i, j){
+    // Get a randomly sampled camera ray for the pixel at location i, j
+    
+    let pixel_center = addVecs(
+          this.pixel00_loc, 
+          p5.Vector.mult(this.pixel_delta_u, i),
+          p5.Vector.mult(this.pixel_delta_v, j)
+    );
+    let pixel_sample = p5.Vector.add(pixel_center, this.pixel_sample_square());
+    
+    let ray_origin = this.center;
+    let ray_direction = p5.Vector.sub(pixel_sample, ray_origin);
+    
+    return new Ray(ray_origin, ray_direction);
+  }
+  
+  pixel_sample_square(){
+    // Returns a random point in the square surrounding a pixel at the origin
+    let px = -0.5 + random();
+    let py = -0.5 + random();
+    return p5.Vector.add(
+      p5.Vector.mult(this.pixel_delta_u, px),
+      p5.Vector.mult(this.pixel_delta_v, py)
+    );
+  }
+}
+
 class Ray{
   constructor(orig, dir){
     this.origin = orig;
--- sketch.js
+++ sketch.js
@@ -5,103 +5,21 @@
 // Ray Tracing in One Weekend
 // https://raytracing.github.io/books/RayTracingInOneWeekend.html
 
-const aspect_ratio = 16 / 9;
-const image_width = 400;
-
-// Calculate the image_height, and ensure that it's at least 1
-var image_height = Math.floor(image_width / aspect_ratio);
-image_height = (image_height < 1) ? 1: image_height;
-
-// World
-
-const world = new HittableList();
-
-world.add(new Sphere(new p5.Vector(0, 0, -1), 0.5));
-world.add(new Sphere(new p5.Vector(0, -100.5, -1), 100));
-
-// Camera
-
-const focal_length = 1.0;
-const viewport_height = 2.0;
-const viewport_width = viewport_height * (image_width / image_height);
-const camera_center = new p5.Vector(0, 0, 0);
-
-// Calulate the vectors across the horizontal and down the vertical viewport edges
-const viewport_u = new p5.Vector(viewport_width, 0, 0);
-const viewport_v = new p5.Vector(0, -viewport_height, 0);
-
-// Calculate the horizontal and vertical delta vectors from pixel to pixel
-const pixel_delta_u = p5.Vector.div(viewport_u, image_width);
-const pixel_delta_v = p5.Vector.div(viewport_v, image_height);
-
-// Calculate the location of the upper left pixel
-const viewport_upper_left = subVecs(
-  camera_center,
-  new p5.Vector(0, 0, focal_length),
-  p5.Vector.div(viewport_u, 2),
-  p5.Vector.div(viewport_v, 2)
-);
-
-const pixel00_loc = p5.Vector.add(
-  viewport_upper_left, 
-  p5.Vector.mult( p5.Vector.add(pixel_delta_u, pixel_delta_v), 0.5 )
-);
-
-// Render
 
 function setup() {  
   
-  createCanvas(image_width, image_height);
-  
-  pixelDensity(1);
-  loadPixels();
-  for (let j = 0; j < image_height; j ++){
-    for (let i = 0; i < image_width; i ++){
+  const world = new HittableList();
 
-      let pixel_center = addVecs(
-        pixel00_loc, 
-        p5.Vector.mult(pixel_delta_u, i),
-        p5.Vector.mult(pixel_delta_v, j)
-      );
-      
-      let ray_direction = p5.Vector.sub(pixel_center, camera_center);
-      
-      let r = new Ray(camera_center, ray_direction);
-      
-      let pixel_color = ray_color(r, world);
-      
-      let index = (j * width + i) * 4;
-      pixels[index + 0] = pixel_color.x * 255;
-      pixels[index + 1] = pixel_color.y * 255;
-      pixels[index + 2] = pixel_color.z * 255;
-      pixels[index + 3] = 255;
-    }
-  }
-  updatePixels();
+  world.add(new Sphere(new p5.Vector(0, 0, -1), 0.5));
+  world.add(new Sphere(new p5.Vector(0, -100.5, -1), 100));
   
+  const cam = new Camera();
+  
+  cam.aspect_ratio = 16.0 / 9.0;
+  cam.image_width = 400;
+  
+  cam.render(world);
 }
-
-// function draw() {
-// }
-
-function ray_color(r, world){
-  let rec = world.hit(r);
-  if (rec.hitAnything){
-    return p5.Vector.mult(
-      p5.Vector.add(rec.nrml, new p5.Vector(1, 1, 1)),
-      0.5
-    );
-  }
-  
-  let unit_direction = p5.Vector.normalize(r.direction);
-  let a = 0.5 * (unit_direction.y + 1);
-  return p5.Vector.lerp(
-    new p5.Vector(1.0, 1.0, 1.0),
-    new p5.Vector(0.5, 0.7, 1.0), 
-    a
-  );
-}
-
 
 // Useful functions
 
@@ -119,4 +37,25 @@ function subVecs(...vecs){
     result.sub(vec);
   }
   return result;
+}
+
+function random_in_unit_sphere(){
+  while (true){
+    let p = p5.Vector.random3D();
+    if (p5.Vector.dot(p) < 1){
+      return p;
+    }  
+  }
+}
+
+function random_unit_vector(){
+  return p5.Vector.normalize(random_in_unit_sphere());
+}
+
+function random_on_hemisphere(nrml){
+  let on_unit_sphere = random_unit_vector();
+  if (p5.Vector.dot(on_unit_sphere, nrml) > 0.0) // In the same hemisphere as the normal
+    return on_unit_sphere;
+  else
+    return p5.Vector.mult(on_unit_sphere, -1);
 }
